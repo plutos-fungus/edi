8295956ca90866e5be041606b872c723d460c5fb
plutos-fungus <rasmus1070@gmail.com>
2021-08-12 23:07:55 +0200

    Just some config things. You can now turn on/off VI mode. But only in the config file. Hasn't been implimented in the actual programm yet)

--- config_handler.py
+++ config_handler.py
@@ -1,18 +1,38 @@
+import sys
+import re
 import yaml
 from yaml.loader import SafeLoader
 # Handle the config-file
 # Please
-class config_handling:
-    config_file = 'config.yml'
-    themes_files = 'configs/theme'
-    language_files = 'configs/language'
 
-    with open(config_file, 'r') as config:
-        config = yaml.load(config, Loader=SafeLoader)
-        language = config['language']
-        theme = config['theme']
+config_file = 'config.yml'
+language_files = 'configs/language'
+themes_files = 'configs/theme'
 
-        print("=== string ===")
-        print(config)
-        print(language)
-        print(theme)
+VI_mode_on = False
+
+with open(config_file, 'r') as config:
+    config = yaml.load(config, Loader=yaml.FullLoader) # Loader=SafeLoader
+    language = ""
+    VI_mode = config['VI_mode']
+    language = config['language']
+    theme = config['theme']
+
+    print("=== Configs ===")
+    for i in VI_mode:
+        if i is not None:
+            print(i)
+            if i == "y":
+                print("VI mode? yes")
+                VI_mode_on = True
+            elif i == "n":
+                print("VI mode? no")
+                VI_mode_on = False
+
+    for i in language:
+        if i is not None:
+            print(i)
+
+    for i in theme:
+        if i is not None:
+            print(i)
