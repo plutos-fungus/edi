a1f916ef9fc70edd8a701e0c3c9040cb9791c53c
plutos-fungus <rasmus1070@gmail.com>
2021-08-20 19:48:14 +0200

    Config handling got a bit more cursed, but it still works. Also added exceptions when no config is found

--- config_handler.py
+++ config_handler.py
@@ -7,29 +7,38 @@ from yaml.loader import SafeLoader
 
 #========================= Viables and paths =========================#
 def getOperators():
-    config_file = 'configs/config.yml'
-    language_files = 'configs/languages/'
-    themes_files = 'configs/theme/'
+    config_file = '~/.config/edi/config.yml'
+    language_files = '~/.config/edi/languages/'
+    themes_files = '~/.config/edi/theme/'
     VI_mode_on = False
     language = ""
 
     #========================= opening the files =========================#
-    with open(config_file, 'r') as config:
-        config = yaml.load(config, Loader=SafeLoader)
-        VI_mode = config['VI_mode']
-        language = config['language']
-        theme = config['theme']
+    try:
+        with open(config_file, 'r') as config:
+            config = yaml.load(config, Loader=SafeLoader)
+            VI_mode = config['VI_mode']
+            language = config['language']
+            theme = config['theme']
+    except FileNotFoundError: 
+        VI_mode = "n"
+        language = []
+        theme = None # TODO: if no theme is set, don't run Themestuff.
+
 
     actualopperators = []
     for i in language:
         if i is not None:
             #print(i)
-            with open(language_files + i, 'r') as language_config:
-                language_config = yaml.load(language_config, Loader=SafeLoader)
-                opperators = language_config['opperators']
-                #print("=== Language ===")
-                #print(language_files + i)
-                language = i
+            try: 
+                with open(language_files + i, 'r') as language_config:
+                    language_config = yaml.load(language_config, Loader=SafeLoader)
+                    opperators = language_config['opperators']
+                    #print("=== Language ===")
+                    #print(language_files + i)
+                    language = i
+            except FileNotFoundError:
+                opperators = []
     for o in opperators:
         o = o + " "
         actualopperators.append(re.sub("^-{3}", " ", o))
